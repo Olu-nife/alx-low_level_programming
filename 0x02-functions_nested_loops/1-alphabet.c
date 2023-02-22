467418a18d0c633c9e38c2e74735f69c3d5f546f
Olu_nife <maryiyanuoluwa2015@gmail.com>
2023-02-22 11:16:30 -0800

    Alphabets_x1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
  * print_alphabet - A program that print the alphabet in lowercase.
- * 
-* Return: Always 0.
+ *
+ * Return: Always 0.
  */
 
 void print_alphabet(void)
 {
 	char alph = 'a';
 
-	while (alph <= 'z')	
+	while (alph <= 'z')
 	{
-	_putchar(alph);	
-	alph++;
-	}	
+		_putchar(alph);
+		alph++;
+	}
 	_putchar('\n');
 }
