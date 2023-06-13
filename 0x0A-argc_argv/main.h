c9dcef60d1221fef96e1e8fe63c6904a58b36ebc
Olu_nife <maryiyanuoluwa2015@gmail.com>
2023-06-12 23:50:55 -0700

    The best argument against democracy is a five-minute conversation with the average voter

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,14 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-<<<<<<< HEAD
 int main (int argc, char *argv[]);
 int _putchar(char c);
 int _atoi(char *s);
 
-=======
-int main(int argc, char *argv[]);
-int _putchar(char c);
-int _atoi(char *s);
->>>>>>> d1009a5d2a05e58df856f943d3915b770c58dfda
 #endif
