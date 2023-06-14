607410acd5dbb93dd6ebf5089ec73fa8c1273d3a
Olu_nife <maryiyanuoluwa2015@gmail.com>
2023-06-14 02:58:30 -0700

    It is what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -10,12 +10,9 @@
  */
 int main(int argc, char *argv[])
 {
-<<<<<<< HEAD
 	printf("%s\n", argv[0]);
 
-=======
 	(void) argc;
 	printf("%s\n", argv[0]);
->>>>>>> d1009a5d2a05e58df856f943d3915b770c58dfda
 	return (0);
 }
