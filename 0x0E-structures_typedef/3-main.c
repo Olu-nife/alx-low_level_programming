43233a67b9ea7c9e8b006843f519f7eee5cc2de9
Olu_nife <maryiyanuoluwa2015@gmail.com>
2023-06-21 14:21:29 -0700

    Most hackers are young because young people tend to be adaptable. As long as you remain adaptable, you can always be a good hacker

--- 0x0E-structures_typedef/3-main.c
+++ 0x0E-structures_typedef/3-main.c
@@ -1,18 +1,43 @@
+#include "function_pointers.h"
+#include <stdlib.h>
 #include <stdio.h>
-#include "dog.h"
-
+#include "3-calc.h"
 /**
- * main - check the code
+ * main - Prints the result of simple operations.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
  * Return: Always 0.
  */
-int main(void)
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-    dog_t my_dog;
+	int num1, num2;
+	char *op;
 
-    my_dog.name = "Poppy";
-    my_dog.age = 3.5;
-    my_dog.owner = "Bob";
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
-    return (0);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	op = argv[2];
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	printf("%d\n", get_op_func(op)(num1, num2));
+
+	return (0);
 }
